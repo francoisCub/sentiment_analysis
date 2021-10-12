7a58cc9be9226e0e388ff3ea902481f2f469bbf9
francoisCub <francois.cubelier@gmx.com>
2021-10-12 09:52:20 +0200

    Simplify  IMDBClass

--- src/data_classes/IMDB.py
+++ src/data_classes/IMDB.py
@@ -11,6 +11,7 @@ class IMDBClass(Dataset):
 
     def __init__(self, train=True, root_dir="./data", transform=None):
         self.imdbs_train, self.imdbs_test= IMDB()
+        self.imdbs = self.imdbs_train if train else self.imdbs_test
         self.root_dir = root_dir
         self.train = train
 
@@ -22,27 +23,23 @@ class IMDBClass(Dataset):
         def yield_tokens(data_iter):
             for _, text in data_iter:
                 yield tokenizer(text)
-        if transform is None:
+        if transform is None and train:
             self.vocab = build_vocab_from_iterator(yield_tokens(self.imdbs_train), specials=[self.unknown, self.pad])
             self.vocab.set_default_index(self.vocab[self.unknown])
         else:
             self.vocab = transform
 
         # Preprocess data
-        self.imdbs_train, self.imdbs_test= IMDB() # iterator has been consumed
+        # self.imdbs_train, self.imdbs_test = IMDB() # iterator has been consumed
         text_pipeline = lambda x: tokenizer(x) # self.vocab(tokenizer(x))
         label_pipeline = lambda x: 1 if x == 'pos' else 0
 
         # Tokenize
-        self.data_train = [[torch.tensor(label_pipeline(label), dtype=torch.long),text_pipeline(item)] for label, item in self.imdbs_train]
-        self.data_test = [[torch.tensor(label_pipeline(label), dtype=torch.long), text_pipeline(item)] for label, item in self.imdbs_test]
+        self.data = [[torch.tensor(label_pipeline(label), dtype=torch.long),text_pipeline(item)] for label, item in self.imdbs]
 
 
     def __len__(self):
-        if self.train:
-            return len(self.data_train)
-        else:
-            return len(self.data_test)
+        return len(self.data)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
@@ -50,18 +47,10 @@ class IMDBClass(Dataset):
         
         text_pipeline = lambda x: torch.tensor(self.vocab(x), dtype=torch.long)
 
-        if self.train:
-            labels = self.data_train[idx][0]
-            if isinstance(idx, int):
-                return text_pipeline(self.data_train[idx][1]), torch.tensor(labels, dtype=torch.long)
-            text_list = [text_pipeline(self.data_train[i][1]) for i in idx]
-            # padded_text = pad_sequence(text_list, batch_first=True)
-        else:
-            labels = self.data_test[idx][0]
-            if isinstance(idx, int):
-                return text_pipeline(self.data_test[idx][1]), torch.tensor(labels, dtype=torch.long)
-            text_list = [text_pipeline(self.data_test[i][1]) for i in idx]
-            # padded_text = pad_sequence(text_list, batch_first=True)
+        labels = self.data[idx][0]
+        if isinstance(idx, int):
+            return text_pipeline(self.data[idx][1]), torch.tensor(labels, dtype=torch.long)
+        text_list = [text_pipeline(self.data[i][1]) for i in idx]
 
         return text_list, torch.tensor(labels, dtype=torch.long)
 

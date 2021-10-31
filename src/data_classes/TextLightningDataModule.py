71ded136b4c0484e70480da53c58d8b5e2028779
francoisCub <francois.cubelier@gmx.com>
2021-10-31 20:57:07 +0100

    Update data classes and add Yelp review dataset

---
+++ src/data_classes/TextLightningDataModule.py
@@ -0,0 +1,56 @@
+from pytorch_lightning import LightningDataModule
+from torch import LongTensor
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, random_split
+from torchtext.datasets import IMDB, YelpReviewFull
+
+from .IMDB import IMDBClass
+from .YelpReview import YelpReviewClass
+
+
+def collate_fn(batch):
+    x = [item[0] for item in batch]
+    lengths = LongTensor(list(map(len, x)))
+    x = pad_sequence(x, batch_first=True)
+    y = LongTensor([item[1] for item in batch])
+    return x, y, lengths
+
+class TextLightningDataModule(LightningDataModule):
+    def __init__(self, vocab, data_dir=".data", batch_size=32, dataset="IMDB", num_workers=0):
+        super().__init__()
+        self.data_dir = data_dir
+        self.vocab = vocab
+        self.batch_size = batch_size
+        self.dataset = dataset
+        self.num_workers = num_workers
+        if self.dataset not in ["IMDB", "Yelp"]:
+            raise ValueError('dataset should be in ["IMDB", "Yelp"]')
+    
+    def prepare_data(self):
+        if self.dataset == "IMDB":
+            IMDB(self.data_dir)
+        else:
+            YelpReviewFull(self.data_dir)
+
+    def setup(self, stage):
+        if stage == "fit" or stage is None:
+            if self.dataset == "IMDB":
+                data_full = IMDBClass(root_dir=self.data_dir, train=True, transform=self.vocab) 
+                self.data_train, self.data_val = random_split(data_full, [22500, 2500])
+            else:
+                data_full = YelpReviewClass(root_dir=self.data_dir, train=True, transform=self.vocab)
+                self.data_train, self.data_val = random_split(data_full, [585000, 65000])
+            
+
+        if stage == "test" or stage is None:
+            self.data_test = IMDBClass(root_dir=self.data_dir, train=False, transform=self.vocab) if self.dataset == "IMDB" else YelpReviewClass(root_dir=self.data_dir, train=False, transform=self.vocab)
+        
+    def train_dataloader(self):
+        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.data_val, batch_size=self.batch_size, shuffle=False, collate_fn=collate_fn, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.data_test, batch_size=self.batch_size, shuffle=False, collate_fn=collate_fn, num_workers=self.num_workers)
+
--- src/data_classes/YelpReview.py
+++ src/data_classes/YelpReview.py
@@ -1,17 +1,16 @@
 from torch.utils.data import Dataset
-from torchtext.datasets import IMDB
+from torchtext.datasets import YelpReviewFull
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 import torch
-from torch.nn.utils.rnn import pad_sequence
 
 
-class IMDBClass(Dataset):
-    """IMDB dataset."""
+class YelpReviewClass(Dataset):
+    """Yelp review full dataset."""
 
     def __init__(self, train=True, root_dir="./data", transform=None):
-        self.imdbs_train, self.imdbs_test= IMDB(root_dir)
-        self.imdbs = self.imdbs_train if train else self.imdbs_test
+        self.yelp_train, self.yelp_test= YelpReviewFull(root_dir)
+        self.yelp = self.yelp_train if train else self.yelp_test
         self.root_dir = root_dir
         self.train = train
 
@@ -24,18 +23,17 @@ class IMDBClass(Dataset):
             for _, text in data_iter:
                 yield tokenizer(text)
         if transform is None and train:
-            self.vocab = build_vocab_from_iterator(yield_tokens(self.imdbs_train), specials=[self.unknown, self.pad])
+            self.vocab = build_vocab_from_iterator(yield_tokens(self.yelp_train), specials=[self.unknown, self.pad])
             self.vocab.set_default_index(self.vocab[self.unknown])
         else:
             self.vocab = transform
 
         # Preprocess data
-        # self.imdbs_train, self.imdbs_test = IMDB() # iterator has been consumed
         text_pipeline = lambda x: tokenizer(x) # self.vocab(tokenizer(x))
-        label_pipeline = lambda x: 1 if x == 'pos' else 0
+        label_pipeline = lambda x: x-1
 
         # Tokenize
-        self.data = [[torch.tensor(label_pipeline(label), dtype=torch.long),text_pipeline(item)] for label, item in self.imdbs]
+        self.data = [[torch.tensor(label_pipeline(label), dtype=torch.long), text_pipeline(item)] for label, item in self.yelp]
 
 
     def __len__(self):
@@ -49,7 +47,7 @@ class IMDBClass(Dataset):
 
         labels = self.data[idx][0]
         if isinstance(idx, int):
-            return text_pipeline(self.data[idx][1]), labels#torch.tensor(labels, dtype=torch.long)
+            return text_pipeline(self.data[idx][1]), torch.tensor(labels, dtype=torch.long)
         text_list = [text_pipeline(self.data[i][1]) for i in idx]
 
         return text_list, torch.tensor(labels, dtype=torch.long)

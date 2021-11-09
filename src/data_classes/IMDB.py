8b927b308a6f076ab2efcf5714ce1548ab8ac632
francoisCub <francois.cubelier@gmx.com>
2021-11-09 11:03:59 +0100

    Add data class and lightning data module for IMDB sentence.

--- src/data_classes/IMDB.py
+++ src/data_classes/IMDB.py
@@ -1,16 +1,16 @@
-from torch.utils.data import Dataset
-from torchtext.datasets import IMDB
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
+import numpy as np
 import torch
-from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset
+from torchtext.data.utils import get_tokenizer
+from torchtext.datasets import IMDB
+from torchtext.vocab import build_vocab_from_iterator
 
 
 class IMDBClass(Dataset):
     """IMDB dataset."""
 
     def __init__(self, train=True, root_dir="./data", transform=None):
-        self.imdbs_train, self.imdbs_test= IMDB(root_dir)
+        self.imdbs_train, self.imdbs_test = IMDB(root_dir)
         self.imdbs = self.imdbs_train if train else self.imdbs_test
         self.root_dir = root_dir
         self.train = train
@@ -18,25 +18,25 @@ class IMDBClass(Dataset):
         self.unknown = "<unk>"
         self.pad = "<pad>"
 
-        # Build vocabulary
+        def text_pipeline(x): return tokenizer(x)
+        def label_pipeline(x): return 1 if x == 'pos' else 0
+
+        # Build vocabulary if needed
         tokenizer = get_tokenizer('basic_english')
+
         def yield_tokens(data_iter):
             for _, text in data_iter:
                 yield tokenizer(text)
         if transform is None and train:
-            self.vocab = build_vocab_from_iterator(yield_tokens(self.imdbs_train), specials=[self.unknown, self.pad])
+            self.vocab = build_vocab_from_iterator(yield_tokens(
+                self.imdbs_train), specials=[self.unknown, self.pad])
             self.vocab.set_default_index(self.vocab[self.unknown])
         else:
             self.vocab = transform
 
-        # Preprocess data
-        # self.imdbs_train, self.imdbs_test = IMDB() # iterator has been consumed
-        text_pipeline = lambda x: tokenizer(x) # self.vocab(tokenizer(x))
-        label_pipeline = lambda x: 1 if x == 'pos' else 0
-
         # Tokenize
-        self.data = [[torch.tensor(label_pipeline(label), dtype=torch.long),text_pipeline(item)] for label, item in self.imdbs]
-
+        self.data = [[torch.tensor(label_pipeline(label), dtype=torch.long), text_pipeline(
+            item)] for label, item in self.imdbs]
 
     def __len__(self):
         return len(self.data)
@@ -44,15 +44,66 @@ class IMDBClass(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
-        text_pipeline = lambda x: torch.tensor(self.vocab(x), dtype=torch.long)
+
+        def text_pipeline(x): return torch.tensor(
+            self.vocab(x), dtype=torch.long)
 
         labels = self.data[idx][0]
         if isinstance(idx, int):
-            return text_pipeline(self.data[idx][1]), labels#torch.tensor(labels, dtype=torch.long)
+            return text_pipeline(self.data[idx][1]), labels
         text_list = [text_pipeline(self.data[i][1]) for i in idx]
 
         return text_list, torch.tensor(labels, dtype=torch.long)
 
     def vocab_size(self):
         return len(self.vocab)
+
+
+class IMDBSentenceClass(Dataset):
+    """IMDB dataset."""
+
+    def __init__(self, train=True, root_dir="./data", format=None):
+        imdbs_train, imdbs_test = IMDB(root_dir)
+        imdbs = imdbs_train if train else imdbs_test
+        self.root_dir = root_dir
+        self.train = train
+
+        self.unknown = "<unk>"
+        self.pad = "<pad>"
+
+        def label_pipeline(x): return torch.tensor(
+            1, dtype=torch.long) if x == 'pos' else torch.tensor(0, dtype=torch.long)
+        self.labels = [label_pipeline(label) for label, _ in imdbs]
+
+        self.format = format
+        if self.format == "bert":
+            if train:
+                self.encoded_text = torch.load(".data/IMDB_bert_train.pt")
+            else:
+                self.encoded_text = torch.load(".data/IMDB_bert_test.pt")
+        elif self.format == "wme":
+            if train:
+                self.encoded_text = np.load(".data/IMDB_wme_train_300.npy")
+                mean = self.encoded_text.mean()
+                std = self.encoded_text.std()
+                self.encoded_text = (self.encoded_text - mean) / std
+            else:
+                self.encoded_text = np.load(".data/IMDB_wme_train_300.npy")
+                # mean and std computed on train set
+                mean = self.encoded_text.mean()
+                std = self.encoded_text.std()
+                self.encoded_text = np.load(".data/IMDB_wme_test_300.npy")
+                self.encoded_text = (self.encoded_text - mean) / std
+
+            self.encoded_text = torch.from_numpy(self.encoded_text).float()
+        else:
+            raise ValueError("Format in 'wme' or 'bert'")
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        return self.encoded_text[idx], self.labels[idx]
--- src/data_classes/TextLightningDataModule.py
+++ src/data_classes/TextLightningDataModule.py
@@ -1,13 +1,15 @@
 from pytorch_lightning import LightningDataModule
-from torch import LongTensor
+from torch import LongTensor, stack
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, random_split
 from torchtext.datasets import IMDB, YelpReviewFull
 
-from .IMDB import IMDBClass
+from .IMDB import IMDBClass, IMDBSentenceClass
 from .YelpReview import YelpReviewClass
 
-def get_collate_fn(trunc=-1):
+
+def get_collate_fn(trunc=-1, format=None):
+
     def collate_fn(batch):
         x = [item[0] for item in batch]
         if trunc > 0:
@@ -16,10 +18,22 @@ def get_collate_fn(trunc=-1):
         x = pad_sequence(x, batch_first=True)
         y = LongTensor([item[1] for item in batch])
         return x, y, lengths
-    return collate_fn
+
+    def collate_fn_sentence(batch):
+        x = stack([item[0] for item in batch])
+
+        lengths = LongTensor(list(map(len, x)))  # not usefull here
+        y = stack([item[1] for item in batch])
+        return x, y, lengths
+
+    if format is None:
+        return collate_fn
+    else:
+        return collate_fn_sentence
+
 
 class TextLightningDataModule(LightningDataModule):
-    def __init__(self, vocab, data_dir=".data", batch_size=32, dataset="IMDB", num_workers=0, trunc=-1):
+    def __init__(self, vocab, data_dir=".data", batch_size=32, dataset="IMDB", num_workers=0, trunc=-1, format=None, shuffle=True):
         super().__init__()
         self.data_dir = data_dir
         self.vocab = vocab
@@ -27,9 +41,13 @@ class TextLightningDataModule(LightningDataModule):
         self.dataset = dataset
         self.num_workers = num_workers
         self.trunc = trunc
-        if self.dataset not in ["IMDB", "Yelp"]:
+        self.shuffle = shuffle
+        self.format = format
+        if self.format is not None and self.dataset != "IMDBSentence":
+            raise ValueError("text is only for IMDBSentence")
+        if self.dataset not in ["IMDB", "Yelp", "IMDBSentence"]:
             raise ValueError('dataset should be in ["IMDB", "Yelp"]')
-    
+
     def prepare_data(self):
         if self.dataset == "IMDB":
             IMDB(self.data_dir)
@@ -39,22 +57,40 @@ class TextLightningDataModule(LightningDataModule):
     def setup(self, stage):
         if stage == "fit" or stage is None:
             if self.dataset == "IMDB":
-                data_full = IMDBClass(root_dir=self.data_dir, train=True, transform=self.vocab) 
-                self.data_train, self.data_val = random_split(data_full, [22500, 2500])
+                data_full = IMDBClass(
+                    root_dir=self.data_dir, train=True, transform=self.vocab, format=self.format)
+                self.data_train, self.data_val = random_split(
+                    data_full, [22500, 2500])
+            elif self.dataset == "Yelp":
+                data_full = YelpReviewClass(
+                    root_dir=self.data_dir, train=True, transform=self.vocab)
+                self.data_train, self.data_val = random_split(
+                    data_full, [585000, 65000])
+            elif self.dataset == "IMDBSentence":
+                data_full = IMDBSentenceClass(
+                    root_dir=self.data_dir, train=False, transform=self.vocab, format=self.format)
+                self.data_train, self.data_val = random_split(
+                    data_full, [22500, 2500])
             else:
-                data_full = YelpReviewClass(root_dir=self.data_dir, train=True, transform=self.vocab)
-                self.data_train, self.data_val = random_split(data_full, [585000, 65000])
-            
+                raise ValueError(
+                    "dataset should be in IMDB, Yelp, IMDBSentence")
 
         if stage == "test" or stage is None:
-            self.data_test = IMDBClass(root_dir=self.data_dir, train=False, transform=self.vocab) if self.dataset == "IMDB" else YelpReviewClass(root_dir=self.data_dir, train=False, transform=self.vocab)
-        
+            if self.dataset == "IMDB":
+                self.data_test = IMDBClass(
+                    root_dir=self.data_dir, train=False, transform=self.vocab, format=self.format)
+            elif self.dataset == "Yelp":
+                YelpReviewClass(root_dir=self.data_dir,
+                                train=False, transform=self.vocab)
+            elif self.dataset == "IMDBSentence":
+                self.data_test = IMDBSentenceClass(
+                    root_dir=self.data_dir, train=False, transform=self.vocab, format=self.format)
+
     def train_dataloader(self):
-        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=True, collate_fn=get_collate_fn(self.trunc), num_workers=self.num_workers)
+        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=self.shuffle, collate_fn=get_collate_fn(self.trunc, format=self.format), num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.data_val, batch_size=self.batch_size, shuffle=False, collate_fn=get_collate_fn(self.trunc), num_workers=self.num_workers)
+        return DataLoader(self.data_val, batch_size=self.batch_size, shuffle=False, collate_fn=get_collate_fn(self.trunc, format=self.format), num_workers=self.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.data_test, batch_size=self.batch_size, shuffle=False, collate_fn=get_collate_fn(self.trunc), num_workers=self.num_workers)
-
+        return DataLoader(self.data_test, batch_size=self.batch_size, shuffle=False, collate_fn=get_collate_fn(self.trunc, format=self.format), num_workers=self.num_workers)

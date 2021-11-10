dc6970182c653f0a81af64ecd931ba249a01b324
Lisa-Byd <l.bueres@student.uliege.be>
2021-11-10 10:31:00 +0100

    pretrained embeds file done, env and gignupdated

--- src/data_classes/pretrained_embeddings.py
+++ src/data_classes/pretrained_embeddings.py
@@ -1,8 +1,14 @@
 import torchtext
 from typing import Tuple
+import gensim.downloader as api
+from gensim.models import KeyedVectors
+import torch
+import torch.nn as nn
+from os.path import exists
+from collections import OrderedDict
 
 def get_pretrained_embeddings(embedding="Glove", max_vectors=10000, dim=300, unk_token="<unk>", unk_index=0, cache=".vector_cache") -> Tuple[torchtext.vocab.Vocab, torchtext.vocab.Vectors]:
-    """Returns pretrianed embeddings
+    """Returns pretrained embeddings
 
     Args:
         embedding (str, optional): Desired embedding in ["Word2Vec", "GloVe", "FastText"]. Defaults to "Glove".
@@ -14,18 +20,34 @@ def get_pretrained_embeddings(embedding="Glove", max_vectors=10000, dim=300, unk
     Returns:
         Tuple[Vocab, Vectors]: vocabulary and corresponding embedding vectors
     """
-    # TODO: implement Word2Vec and FastText. https://pytorch.org/text/stable/vocab.html 
+
     if embedding == "Glove":
         vectors = torchtext.vocab.GloVe(max_vectors=max_vectors, cache=cache, dim=dim)
         vocab = torchtext.vocab.vocab(vectors.stoi)
         vocab.insert_token(unk_token, unk_index)
         vocab.set_default_index(vocab[unk_token])
     elif embedding == "Word2Vec":
+        if not exists(cache+'/word2vec-google-news-300'):
+            vectors_first = (api.load('word2vec-google-news-300'))
+            #vectors_first = KeyedVectors.load_word2vec_format(cache+"/GoogleNews-vectors-negative300.bin", binary=True)
+            vectors_first.save(cache+'/word2vec-google-news-300')
+        model = KeyedVectors.load(cache+'/word2vec-google-news-300')
+        weights = torch.FloatTensor(model.vectors)
+        vectors = nn.Embedding.from_pretrained(weights)
+        vectors.requires_grad = False
+        ordered_dict = OrderedDict(model.key_to_index)
+        vocab = torchtext.vocab.vocab(ordered_dict)
+        vocab.insert_token(unk_token, unk_index)
+        vocab.set_default_index(vocab[unk_token])
+
         # TODO (search... https://clay-atlas.com/us/blog/2021/08/06/pytorch-en-use-nn-embedding-load-gensim-pre-trained-weights/ ?)
         # At least the vectors
         pass
     elif embedding == "FastText":
-        # TODO (like glove)
+        vectors = torchtext.vocab.FastText(max_vectors=max_vectors, cache=cache)
+        vocab = torchtext.vocab.vocab(vectors.stoi)
+        vocab.insert_token(unk_token, unk_index)
+        vocab.set_default_index(vocab[unk_token])
         pass
 
     return vocab, vectors
